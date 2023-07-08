2f35b0084a2e96709d698d88787646321bb49a91
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-07-08 19:41:19 +0300

    fix lazy

--- src/components/Services/Api.js
+++ src/components/Services/Api.js
@@ -1,15 +1,11 @@
 const BASE_URL = 'https://api.themoviedb.org/';
 const API_KEY = 'd871c37f03af5f9d732b89f21e2b613f';
 
-export const fetchQueryMovie = async (query) => {
-  try {
-    const response = await fetch(`${BASE_URL}${query}api_key=${API_KEY}`);
-    if (response.ok) {
-      return await response.json();
-    } else {
-      throw new Error('Error');
-    }
-  } catch (error) {
-    console.log(error);
+
+export const fetchQueryMovie = async query => {
+  const response = await fetch(`${BASE_URL}${query}api_key=${API_KEY}`);
+  if (response.ok) {
+    return response.json();
   }
+  return await Promise.reject(new Error('error'));
 };
