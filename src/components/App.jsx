fbb141fae4fb418639c4a8fa98f03e9e0cfd1b0d
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-07-08 19:23:39 +0300

    lazy push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route} from "react-router-dom";
-import Home from "pages/Home/Home";
-import Movies from "pages/Movies/Movies";
-import MovieDetails from "pages/MovieDetails/MovieDetails";
-import Layout from 'components/Layout/Layout';
-import Cast from "./Cast/Cast";
-import Reviews from "./Reviews/Reviews";
+import { lazy } from "react";
 import css from './App.module.css'
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Layout = lazy(() => import('components/Layout/Layout'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 
 
